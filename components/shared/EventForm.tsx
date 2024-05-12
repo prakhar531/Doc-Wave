af10747c5e61ddf60371e24dd90679f8b5e1e3ea
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-05-13 00:18:58 +0530

    Added rozorpay

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -37,13 +37,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import { UploadButton, UploadDropzone, Uploader } from "@/lib/uploadthing";
 import { FileText, Pencil, Plus } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { createCategory } from "@/lib/actions/category.actions";
 
-import sha256 from "crypto-js/sha256";
-import { redirect } from "next/navigation";
-import axios from "axios";
-import { v4 as uuidv4 } from "uuid";
-import path from "path";
 import { createOrder } from "@/lib/actions/order.actions";
 
 type EventFormProps = {
@@ -56,58 +50,6 @@ const EventForm = ({ userId }: EventFormProps) => {
   const [pdfPrice, setPdfPrice] = useState("");
   const [formData, setFormData] = useState({});
   let finalPrice = 0;
-
-  //phone pay
-  const makePayment = async () => {
-    const transactionid = "Tr-" + uuidv4().toString().slice(-6);
-
-    const payload = {
-      merchantId: process.env.NEXT_PUBLIC_MERCHANT_ID,
-      merchantTransactionId: transactionid,
-      merchantUserId: "MUID-" + uuidv4().toString().slice(-6),
-      amount: 10000,
-      redirectUrl: `http://localhost:3000/api/status/${transactionid}`,
-      redirectMode: "POST",
-      callbackUrl: `http://localhost:3000/api/status/${transactionid}`,
-      mobileNumber: "9999999999",
-      paymentInstrument: {
-        type: "PAY_PAGE",
-      },
-    };
-
-    const dataPayload = JSON.stringify(payload);
-    console.log(dataPayload);
-
-    const dataBase64 = Buffer.from(dataPayload).toString("base64");
-    console.log(dataBase64);
-
-    const fullURL =
-      dataBase64 + "/pg/v1/pay" + process.env.NEXT_PUBLIC_SALT_KEY;
-    const dataSha256 = sha256(fullURL);
-
-    const checksum = dataSha256 + "###" + process.env.NEXT_PUBLIC_SALT_INDEX;
-    console.log("c====", checksum);
-
-    const UAT_PAY_API_URL =
-      "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay";
-
-    const response = await axios.post(
-      UAT_PAY_API_URL,
-      {
-        request: dataBase64,
-      },
-      {
-        headers: {
-          accept: "application/json",
-          "Content-Type": "application/json",
-          "X-VERIFY": checksum,
-        },
-      }
-    );
-
-    const redirect = response.data.data.instrumentResponse.redirectInfo.url;
-    router.push(redirect);
-  };
 
   //create order
 
@@ -140,7 +82,7 @@ const EventForm = ({ userId }: EventFormProps) => {
       if (newOrder) {
         form.reset();
       }
-      router.push(`/orders/${newOrder._id}`);
+      router.push(`/checkout/?amount=${+pdfPrice}`);
     } catch (error) {
       console.log(error);
     }
