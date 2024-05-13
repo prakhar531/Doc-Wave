79396d1f2cc031fd3d98437071a33eb1aff21a23
Prakhar Arya <139108232+prakhar531@users.noreply.github.com>
2024-05-13 11:12:19 +0530

    updated design

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -31,17 +31,21 @@ export default async function Home({ searchParams }: SearchParamProps) {
 
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper flex flex-col gap-5 md:flex-row 2xl:gap-0">
-          <div className="flex flex-col justify-center gap-8 w-full md:w-1/2 xl:w-3/5">
+          <div className="flex flex-col justify-center gap-2 w-full md:w-1/2 xl:w-3/5">
             <h1 className="h1-bold">Printing Magic At Your Fingertips</h1>
-            <p className="p-regular-20 md:p-regular-24">
-              Book and learn helpful tips from 3,168+ mentors in world-class
-              companies with our global community.
+            <p className="p-regular-16 md:p-regular-20 mt-6">
+              A web platform essential for safe and convenient services,
+              connecting and revolutionizing document systems for students.
+            </p>
+            <p className="p-regular-16 md:p-regular-20 mb-6">
+              DOC-WAVE facilitates easy document handling, enhancing
+              productivity and operational efficiency in the digital landscape.
             </p>
             <SignedOut>
               <Button
                 size="lg"
                 asChild
-                className="button w-full sm:w-fit bg-[#1e3262] hover:bg-[#6385a3]"
+                className="button w-full sm:w-fit bg-[#1e3262] hover:bg-[#6385a3] mt-6"
               ></Button>
             </SignedOut>
             <SignedIn>
